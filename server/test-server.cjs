253271ac99d3ed5b3dcb42ecea09962bd6d8491d
kayky isidio dos santos <kayky@MacBook-Air-de-kayky.local>
2026-05-31 12:35:43 -0300

    chore: remove test file

--- server/test-server.cjs
+++
@@ -1,4 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('/api/ping', (req, res) => res.json({ pong: true }));
-app.listen(process.env.PORT || 3001, () => console.log('TEST server on ', process.env.PORT || 3001));
